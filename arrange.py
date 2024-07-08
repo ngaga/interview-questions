2f1a6dd95823f9b472898109d6eb4548e136a4c8
ngaga <nicolasgms6@gmail.com>
2024-07-08 15:19:55 +0200

    Update arrange.py
    use codeBuddy from Dust to debug

--- arrange.py
+++ arrange.py
@@ -1,9 +1,31 @@
+#import sys
+#i = 0
+#i = i+1
+    #print(i)
+    #if i > 10:
+    #    sys.exit()
+
 def arrange(remain, build):
+    
     if (len(build) == 3):
         print(build)
     else:
-        for i in range(remain):
-            arrange(remain - remain[i], build + remain[i])
+        for elements in remain:
+            buildCopy = build + remain[-1:]
+            remainCopy = remain[:-1]
+            arrange(remainCopy, buildCopy)
+
+
+
+
+def arrange2(remain, build):
+    if len(build) == 3:
+        print(build)
+    else:
+        for i in range(len(remain)):
+            new_build = build + [remain[i]]
+            new_remain = remain[:i] + remain[i+1:]
+            arrange(new_remain, new_build)
     
 
-arrange(["a, b, c"], [])
+arrange2([1, 2, 3], [])

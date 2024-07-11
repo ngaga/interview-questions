4d6640ab4535675428a40ba68fc44f976c89eef3
ngaga <nicolasgms6@gmail.com>
2024-07-11 17:24:19 +0200

    arrange.rs : fix borrowing

--- arrange.rs
+++ arrange.rs
@@ -1,43 +1,20 @@
-fn arrange(remain: &mut Vec<i32>, build: &mut Vec<i32>) {
+fn arrange(remain: &Vec<i32>, build: &Vec<i32>) {
     if remain.is_empty() {
         println!("{:?}", build);
     } else {
         for i in 0..remain.len() {
-            let mut build_copy = build.clone();
-            build_copy.push(remain[i]);
-            let mut remain_copy = remain.clone();
-            remain_copy.remove(i);
-            arrange(&mut remain_copy, &mut build_copy);
-        }
-    }
-}
-
-fn main() {
-    let mut data = vec![1, 2, 3, 4];
-    let mut result = vec![];
-    arrange(&mut data, &mut result);
-}
-
-
-
-fn arrange(remain: &mut Vec<i32>, build: &mut Vec<i32>) {
-    if remain.is_empty() {
-        println!("{:?}", build);
-    } else {
-        for i in 0..remain.len() {
-            let mut build_copy = build.clone();
+            let mut build_copy: Vec<i32> = build.clone();
             let last = remain[i];
             build_copy.push(last);
-            let mut remain_copy = remain.clone();
+            let mut remain_copy: Vec<i32> = remain.clone();
             remain_copy.remove(i);
-            arrange(&remain_copy, build_copy);
+            arrange(&remain_copy, &build_copy);
         }
     }
 }
 
 fn main() {
-    let mut data = vec![1, 2, 3, 4];
-    let mut result = vec![];
-    arrange(&mut data, &mut result);
-  
+    let data = vec![1, 2, 3];
+    let result = vec![];
+    arrange(&data, &result);
 }

836ddc5bf88314228d8290f4be62230efe896a49
ngaga <nicolasgms6@gmail.com>
2024-09-23 14:35:03 +0200

    wip

--- extra_caracters_test.py
+++ extra_caracters_test.py
@@ -21,14 +21,37 @@
 # Explanation: We can break s in two substrings: "hello" from index 3 to 7 and
 #  "world" from index 8 to 12. The characters at indices 0, 1, 2 are not used in any substring and thus are considered as extra characters. Hence, we return 3.
 
-def extra(s, dictionnary):
-    return 0
-
 import unittest
+
+# check if a word is a part of s
+def isPartAtIndex(word, s):
+    for i in range(0, len(s) - len(word)):
+        if s[i:i+len(word)] == word:
+            return
+    return None
+
+class TestPart(unittest.TestCase):
+    def testPartOne(self):
+        
+
+# Solves the exercive above
+def extra(s, dictionnary):
+    count = 0
+    # index for the string s iteration
+    i  = 0
+    while i < len(s):
+        # first test only with the first word of the dictionnary
+        a = dictionnary[0]
+        while 
+        i +=1
+        count +=1
+    return count
+
 
 class TestExtra(unittest.TestCase):
     def testOne(self):
         self.assertEqual(extra("leetscode", ["leet","code","leetcode"]), 1)
+    def testTwo(self):
         self.assertEqual(extra("sayhelloworld", ["hello","world"]), 3)
 
 if __name__ == '__main__':

b82cd5a28c4c3d48149faf114f85da2aa1b87b81
ngaga <nicolasgms6@gmail.com>
2025-01-06 11:58:05 +0100

    Update leetcode1769.py

--- leetcode1769.py
+++ leetcode1769.py
@@ -1,19 +1,10 @@
-def moveAllToIethPosition2(input : str, targetPosition : int) -> int:
+def moveAllToIethPosition(input : str, targetPosition : int) -> int:
     return sum([abs(i - targetPosition) 
         for i, v in enumerate(input) if v == '1'])
 
-
-def moveAllToIethPosition(input : str, targetPosition : int) -> int:
-    result = 0 
-    for i, v in enumerate(input):
-        if v == "1":
-            result += abs(i - targetPosition)
-    return result
-
 def computeSolutionVector(input : str) -> list[int]:
     #TODO: comment on aurait fait pour renvoyer un string?
-    return [moveAllToIethPosition2(input[:], i) for i in range(len(input))]
-    
+    return [moveAllToIethPosition(input[:], i) for i in range(len(input))]
 
 print(computeSolutionVector("110"))
 #Output: [1,1,3]

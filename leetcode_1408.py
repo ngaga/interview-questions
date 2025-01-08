1e7726c1d35dc0d53becced0d5351e09377d2247
ngaga <nicolasgms6@gmail.com>
2025-01-08 12:43:12 +0100

    Update leetcode_1408.py

--- leetcode_1408.py
+++ leetcode_1408.py
@@ -1,4 +1,4 @@
-def isWordInList(wordList : list[str], target : str) -> Bool:
+def isWordInList(wordList : list[str], target : str) -> bool:
     for w in wordList:
         if target in w:
             return True
@@ -7,12 +7,16 @@ def isWordInList(wordList : list[str], target : str) -> Bool:
 def wordInOthers(wordList : list[str]) -> list[str]:
     result = []
     for word in wordList:
-        listExtract = wordList[:]
+        listExtract = wordList.copy()
         listExtract.remove(word)
-        
+        if isWordInList(listExtract, word):
+            result.append(word)   
     return result
 
 
-print
-# print(wordInOthers(["mass","as","hero","superhero"]))
+print(wordInOthers(["mass","as","hero","superhero"]))
 # Output: ["as","hero"]
+print(wordInOthers(["leetcode","et","code"]))
+# Output: ["et","code"]
+print(wordInOthers(["blue","green","bu"]))
+# Output: []

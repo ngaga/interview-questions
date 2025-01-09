f205814a1f1e4b590d1ef241c38189178ac86eea
ngaga <nicolasgms6@gmail.com>
2025-01-09 14:28:19 +0100

    Update leetcode_2185.py

--- leetcode_2185.py
+++ leetcode_2185.py
@@ -1,5 +1,5 @@
 def countWordsWithPrefix(words : list[str], prefix : str) -> int:
-    return sum([1 for word in words if word.startswith(prefix)])
+    return sum(1 for word in words if word.startswith(prefix))
 
 
 print(

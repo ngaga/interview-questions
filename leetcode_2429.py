bbdcd93cccefb087dc5d8215e90c8bdfe9691091
ngaga <nicolasgms6@gmail.com>
2025-01-21 12:00:38 +0100

    Create leetcode_2429.py

---
+++ leetcode_2429.py
@@ -0,0 +1,11 @@
+def minXor(numXor : int, numBits : int) -> int:
+    if numBits < numXor:
+        
+        return 
+    print(bin(numBits))
+    print(numBits ^ numXor)
+    return 0
+
+print(minXor(3, 3))
+# expect 3
+

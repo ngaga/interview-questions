270bfd4d88018bb2aefe41bdc162c0b6b9947db3
ngaga <nicolasgms6@gmail.com>
2026-02-04 17:25:59 +0100

    Refactor justify function and add build_line helper

--- text_justification_leet_68.py
+++ text_justification_leet_68.py
@@ -1,22 +1,33 @@
-from math import floor
 
-def justify(words, maxWidth):
+
+def build_line(words, current_index, max_width):
+    word_next_index = current_index
+    line = ""
+    while len(line) < max_width and word_next_index < len(words):
+        preview_size = len(words[word_next_index]) + len(line)
+        if preview_size > max_width:
+            print("Biiiig ", preview_size)
+            break
+        else:
+            line += words[word_next_index]
+            line += " "
+            word_next_index += 1
+    return line, word_next_index
+
+def justify(words, max_width):
     result = []
-    for w in words:
-        remaining_spaces = maxWidth - len(w)
-        d_left = floor(remaining_spaces / 2)
-        d_right = d_left
-        if remaining_spaces % 2 != 0:
-            d_left = d_left + 1
-        print(d_left, d_right)
-        line = ' ' * d_left + w + ' ' * d_right
+    i = 0
+    while i < len(words):
+        line , i = build_line(words, i, max_width)
+        print(f"len(line) {len(line)}")
         result.append(line)
     return result
 
 
 words = ["This", "is", "an", "example", "of", "text", "justification."]
-maxWidth = 16
+max_width = 16
 
-justified = justify(words, maxWidth)
+justified = justify(words, max_width)
 print(*justified, sep='|\n')
+
 

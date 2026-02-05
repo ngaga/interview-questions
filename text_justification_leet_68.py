f92d61974298a25aeec417c43d331ad69dffcca4
ngaga <nicolasgms6@gmail.com>
2026-02-05 16:01:52 +0100

    Refactor text justification logic and update test words

--- text_justification_leet_68.py
+++ text_justification_leet_68.py
@@ -18,12 +18,24 @@ class Line:
 
     def stretch(self, max_width):
         number_of_spaces_to_add = max_width - self.size()
-        print(f"number_of_spaces_to_add {number_of_spaces_to_add}")
+        word_count = len(self.words)
+        print(f"number_of_spaces_to_add {number_of_spaces_to_add}, word_count: {word_count}")
+        if word_count == 1:
+            self.words[0].number_of_spaces_to_add = number_of_spaces_to_add
+        else:
+            # If n words there are n-1 spaces in between.
+            interval_count = word_count - 1
+            minimum_of_spaces_per_separation = 1 + number_of_spaces_to_add // interval_count
+            remaining_spaces_to_place = number_of_spaces_to_add % interval_count
+            print("quotien, reste ", minimum_of_spaces_per_separation, remaining_spaces_to_place)
+            for w in self.words:
+                w.number_of_spaces_to_add = minimum_of_spaces_per_separation
+            self.clean_last_space()
+            for i in range(remaining_spaces_to_place):
+                self.words[i].number_of_spaces_to_add += 1
 
     def clean_last_space(self):
-        self.words[-1].number_of_spaces_to_add = 0
-
-        
+        self.words[-1].number_of_spaces_to_add = 0        
 
     def size(self):
         return sum(w.size() for w in self.words)
@@ -42,8 +54,8 @@ def build_line(words, current_index, max_width):
         else:
             line.words.append(WordAndFollowingSpaces(words[word_next_index]))
             word_next_index += 1
-        line.clean_last_space()
-        line.stretch(max_width)
+    line.clean_last_space()
+    line.stretch(max_width)
     return line, word_next_index
 
 def justify(words, max_width):
@@ -56,8 +68,11 @@ def justify(words, max_width):
     return result
 
 
-words = ["This", "is", "an", "example", "of", "text", "justification."]
-max_width = 16
+# words = ["This", "is", "an", "example", "of", "text", "justification."]
+# words = ["What","must","be","acknowledgment","shall","be"]
+# max_width = 16
+words = ["Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"]
+max_width = 20
 
 justified = justify(words, max_width)
 print(*justified, sep='|\n')

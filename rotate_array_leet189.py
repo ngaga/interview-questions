c9c377f19b35705272d9af6577a166c10cfae869
ngaga <nicolasgms6@gmail.com>
2026-02-10 10:14:17 +0100

    Fix reverse logic and adjust rotation handling

--- rotate_array_leet189.py
+++ rotate_array_leet189.py
@@ -1,21 +1,15 @@
 def rotate(nums, k):
-    def reverse_between_indexes(nums, i, j):
-        while i < j:
-            try:
-                nums[i], nums[j] = nums[j], nums[i]
-            except:
-                print(f"error: i, j {i} {j}")
-                return
+   def reverse_between_indexes(nums, i, j):
+        while i <= j:
+            nums[i], nums[j] = nums[j], nums[i]
             i += 1
             j -= 1
-
     l = len(nums)
-    reverse_between_indexes(nums, 0 , l - 1)
-    print(f" l - 1, nums {l -1} {nums}")
+    k = k % l
+    reverse_between_indexes(nums,0 , l - 1)
     reverse_between_indexes(nums, 0, k - 1)
-    print(nums)
     reverse_between_indexes(nums, k, l - 1)
-    print(nums)
+    
 
 
 nums = [-1]
